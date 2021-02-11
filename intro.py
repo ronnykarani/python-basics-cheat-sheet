8e847bbe891b966fc5f3958ddae0d17d3b5e7c56
ronny karani <karanironny25@gmail.com>
2021-02-11 11:19:37 +0300

    added more notes

--- intro.py
+++ intro.py
@@ -138,9 +138,89 @@ print(look_one[-3])
 #add : btwn the range (starting from the fast to the second)
 #the character in the second index is not returned
 print(look_one[3:8])
+#if you dont key in the end index python returns all characters to the end of the string
+print(look_one[3:])
+#if you dont key in the start index python takes it as a zero and returns characters from the zero index 
+print(look_one[:3])
+#if both index are left blank python takes the first index as a zero and go all the way to end
+#its just like copying the whole string
+another = look_one[:]
+print(another)
+
+new_name = 'joan kanana'
+print(new_name[1:-1]) 
+#**************************
 
 
+#**************************
+#FORMATTED STRINGS
+#Useful where you want to generate dynamic text with variables
+first_name = 'john'
+last_name = 'smith'
+message = first_name + ' [' + last_name + '] is a coder '
+print(message)
+#above code is not ideal for complicated texts since it becomes harder to visaulize the output
+#there is where formatted strings come in handy
+#formatted strings are prefixed with an f
+msg =  f'{first_name} [{last_name}] is a coder'
+#{} define placeholders/holds in our strings
+#holds will be filled by values of our variables when we run the program
+print(msg)
+#**************************
 
 
+#**************************
+#STRING METHODS
+lesson = 'Python For Beginners'
+#len is used to calculate the length of the variable
+print(len(lesson))
+#white spaces will also be treated as characters
+#len function can be used to enforce a limit on the number of characters 
+#len is a general purpose function 
+
+#other functions are only specific to strings also known as methods
+# eg  upper and lower
+#when a function is specific to an object we refer to it as method
+#upper and lower are methods since the belong to the string unlike len and print 
+#to access this functions we use the dot operator(.)
+#.upper and .lower converts the string to either of the cases
+print(lesson.upper())
+print(lesson.lower())
+#the method upper does not modify our string it just creates a new string and returns it
+print(lesson)
+# title method is used toconvert the first character in each word to uppercaseand remaining characters to lowercase
+#title method just like upper and lower returns a new string
+my_title = 'the lion is out'
+print(my_title.title())
+print(my_title)
+
+#finding a sequence of characters in a string
+#the find method is used
+#find method is case sensitive
+#when a character thats not in the string is passed a negative will be returned
+print(lesson.find('P'))
+print(lesson.find('g'))
+print(lesson.find('O'))
+print(lesson.find('Python'))
+print(lesson.find('Beginners'))
+#find will return the index of the first appearance of the defined character in the string
+#if a sequence is passed find method will return the first index, where the sequence starts
+
+#replace method is used to replace characters in the string
+#the method will take two arguments (character to replace, new character)
+print(lesson.replace('Beginners','Absolute Beginners'))
+#the method is also case sensitive
+print(lesson.find('P', 'J'))
+
+#another sequence is to check existence of a character or sequence in a string
+#in operator is used
+#in operator is a boolean expression since it returns either true or false
+my_name = 'Ronny Karani Mwirigi'
+print('Ronny' in my_name)
+print('ronny' in my_name)
+#in operator is also is also case sensitive
+#**************************
 
 
+#**************************
+#ARITHMETIC OPERATIONS
